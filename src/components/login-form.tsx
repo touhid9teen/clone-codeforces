1a7cc3533d05f4872617071e6c9f0d41f1d3eea3
touhidul.islam <touhidul.islam@upaybd.com>
2025-08-11 11:56:18 +0600

    initial commmit , creating loginform

---
+++ src/components/login-form.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Card, CardContent, CardHeader } from "./ui/card";
+
+export default function LoginForm() {
+  return (
+    <div>
+      <Card>
+        <CardHeader>Login into Codeforces</CardHeader>
+
+        <CardContent></CardContent>
+      </Card>
+    </div>
+  );
+}
